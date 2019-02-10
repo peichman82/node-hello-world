280ef48bdc2a2fcc4f8aa76f9e93c104cb7d0df4
Miguel Martínez de Paz <miguel.martinez@vass.es>
2019-02-10 09:26:11 +0100

    Hello world node app

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/hello-world', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(8090, function () {
+  console.log('Example app listening on port 8090!');
+});
